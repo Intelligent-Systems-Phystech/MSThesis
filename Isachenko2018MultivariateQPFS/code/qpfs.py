daab71c4c526deaf52972f8d5906b9cf56131324
romanisachenko <isa-ro@yandex.ru>
2018-05-13 23:20:34 +0300

    +1 alg and new structure

--- Isachenko2018MultivariateQPFS/code/qpfs.py
+++ Isachenko2018MultivariateQPFS/code/qpfs.py
@@ -9,7 +9,6 @@ def corr(X, Y=None, fill=0):
         Y = X
     if len(Y.shape) == 1:
         Y = Y[:, np.newaxis]
-        Y = np.atleast_2d(Y)
     if len(X.shape) == 1:
         X = X[:, np.newaxis]
     
@@ -85,14 +84,15 @@ class QPFS:
 
 
 class MultivariateQPFS():
-    def __init__(self):
-        pass
+    def __init__(self, mode='RelAgg'):
+        self.mode = mode
     
-    def get_params(self, X, Y, eps=1e-12):
+    def get_params(self, X, Y):
+        self.m, self.n = X.shape
+        self.r = Y.shape[1] if len(Y.shape) > 1 else 1
+        
         self.Qx = np.abs(corr(X, fill=1))
         self.Qy = np.abs(corr(Y, fill=1))
-        np.fill_diagonal(self.Qx, 0)
-        np.fill_diagonal(self.Qy, 0)
         self.B = np.abs(corr(X, Y))
 
     def get_alpha(self, alpha3=None):
@@ -100,8 +100,8 @@ class MultivariateQPFS():
             alpha3 = np.mean(self.Qx) * np.mean(self.B) / (np.mean(self.Qx) * np.mean(self.B) + 
                                                            np.mean(self.Qx) * np.mean(self.Qy) + 
                                                            np.mean(self.Qy) * np.mean(self.B))
-        alpha1 = (1 - alpha3) * np.mean(self.B) / (np.mean(self.Qx) + np.mean(self.B))
-        alpha2 = (1 - alpha3) * np.mean(self.Qx) / (np.mean(self.Qx) + np.mean(self.B))
+        alpha1 = (1. - alpha3) * np.mean(self.B) / (np.mean(self.Qx) + np.mean(self.B))
+        alpha2 = (1. - alpha3) * np.mean(self.Qx) / (np.mean(self.Qx) + np.mean(self.B))
         return np.array([alpha1, alpha2, alpha3])
 
 
@@ -111,75 +111,41 @@ class MultivariateQPFS():
         self.solve_problem(alphas)
     
     def solve_problem(self, alphas):
-        n = self.Qx.shape[0]
-        r = self.Qy.shape[0]
-        
+        if self.mode == 'SymImp':
+            self._symimp(alphas)
+        elif self.mode == 'MaxMin':
+            self._maxmin(alphas)
+        elif self.mode == 'MinMax':
+            self._minmax(alphas)
+        elif self.mode == 'MaxRel':
+            self._maxrel(alphas)
+        elif self.mode == 'MinMax2':
+            self._minmax2(alphas)
+    
+    def _symimp(self, alphas):
+        # Parameters
         Q = np.vstack((np.hstack((alphas[0] * self.Qx, -alphas[1] / 2 * self.B)),
                        np.hstack(( -alphas[1] / 2 * self.B.T, alphas[2] * self.Qy))))
         Q, lamb_min = shift_spectrum(Q)
         
-        a = cvx.Variable(n + r)
+        c = np.zeros((2, self.n + self.r))
+        c[0, :self.n] = 1
+        c[1, self.n:] = 1
         
-        c = np.zeros((2, n + r))
-        c[0, :n] = 1
-        c[1, n:] = 1
-        
-        objective = cvx.Minimize(cvx.quad_form(a, Q))
-        constraints = [a >= 0, c * a == 1]
-        prob = cvx.Problem(objective, constraints)
-
+        # Problem
+        z = cvx.Variable(self.n + self.r)
+        obj = cvx.Minimize(cvx.quad_form(z, Q))
+        constr = [z >= 0, c * z == 1]
+        prob = cvx.Problem(obj, constr)
         prob.solve()
 
+        # Results
         self.status = prob.status
-        self.score = np.array(a.value).flatten()
-        
-    def get_topk_indices(self, k=10):
-        return self.score[self.B.shape[0]].argsort()[::-1][:k] 
+        self.zx = np.array(z.value).flatten()
+        self.zy = np.ones(self.r) / self.r
     
-
-class MinimaxQPFS():
-    def __init__(self, mode='minmax'):
-        self.mode = mode
-    
-    def get_params(self, X, Y):
-        self.Qx = np.abs(corr(X, fill=1))
-        self.Qy = np.abs(corr(Y, fill=1))
-        self.B = np.abs(corr(X, Y))
-
-    def get_alpha(self, alpha3=None):
-        if alpha3 is None:
-            alpha3 = np.mean(self.Qx) * np.mean(self.B) / (np.mean(self.Qx) * np.mean(self.B) + 
-                                                           np.mean(self.Qx) * np.mean(self.Qy) + 
-                                                           np.mean(self.Qy) * np.mean(self.B))
-        alpha1 = (1 - alpha3) * np.mean(self.B) / (np.mean(self.Qx) + np.mean(self.B))
-        alpha2 = (1 - alpha3) * np.mean(self.Qx) / (np.mean(self.Qx) + np.mean(self.B))
-        return np.array([alpha1, alpha2, alpha3])
-
-
-    def fit(self, X, Y):
-        self.get_params(X, Y)
-        alphas = self.get_alpha()
-        self.solve_problem(alphas)
-    
-    def solve_problem(self, alphas):
-        n = self.Qx.shape[0]
-        r = self.Qy.shape[0]
-        
-        np.fill_diagonal(self.Qx, 0)
-        np.fill_diagonal(self.Qy, 0)
-        self.Qx, lamb_min = shift_spectrum(self.Qx, eps=1e-6)
-        self.Qy, lamb_min = shift_spectrum(self.Qy, eps=1e-6)
-        
-        if self.mode == 'maxmin':
-            self._maxmin(alphas)
-        elif self.mode == 'minmax':
-            self._minmax(alphas)
-        elif self.mode == 'dual_woy':
-            self._dual_woy(alphas)
-        
     def _minmax(self, alphas):
-        n = self.Qx.shape[0]
-        r = self.Qy.shape[0]
+        # Parameters
         Qyinv = np.linalg.pinv(self.Qy)
         Q1 = alphas[1] ** 2 * self.B.dot(Qyinv).dot(self.B.T) + 4 * alphas[0] * alphas[2] * self.Qx
         Q2 = Qyinv
@@ -196,30 +162,25 @@ class MinimaxQPFS():
         
         Q, lamb_min = shift_spectrum(Q)
         
-        a = cvx.Variable(n + r + 1)
+        c = np.zeros(self.n + self.r + 1)
+        c[:self.n] = 1
         
-        c = np.zeros(n + r + 1)
-        c[:n] = 1
-        
-        objective = cvx.Minimize(cvx.quad_form(a, Q) - 4 * alphas[2] * a[-1])
-        constraints = [a[:-1] >= 0, c * a == 1]
-        prob = cvx.Problem(objective, constraints)
-
+        # Problem
+        zx = cvx.Variable(self.n + self.r + 1)
+        obj = cvx.Minimize(cvx.quad_form(zx, Q) - 4 * alphas[2] * zx[-1])
+        constr = [zx[:-1] >= 0, c * zx == 1]
+        prob = cvx.Problem(obj, constr)
         prob.solve(solver=cvx.CVXOPT)
-
-        score = np.array(a.value).flatten()
-        self.lamb = score[-1]
-        self.nu = score[n:-1]
-        self.ax = score[:n]
-        self.ay = 1. / (2 * alphas[2]) * Qyinv.dot(-alphas[1] * self.B.T.dot(self.ax) + score[-1] + score[n:-1])
         
-        self.Q = Q
-        self.score = score
+        # Results
+        score = np.array(zx.value).flatten()
+        self.status = prob.status
+        self.zx = score[:self.n]
+        self.zy = 1. / (2 * alphas[2]) * Qyinv.dot(-alphas[1] * self.B.T.dot(self.zx) + 
+                                                   score[-1] + score[self.n:-1])
     
     def _maxmin(self, alphas):
-        n = self.Qx.shape[0]
-        r = self.Qy.shape[0]
-        
+        # Parameters
         Qxinv = np.linalg.pinv(self.Qx)
         Q1 = alphas[1] ** 2 * self.B.T.dot(Qxinv).dot(self.B) + 4 * alphas[0] * alphas[2] * self.Qy
         Q2 = Qxinv
@@ -235,108 +196,94 @@ class MinimaxQPFS():
         ])
         
         Q, lamb_min = shift_spectrum(Q)
-        self.lamb_min = lamb_min
         
-        a = cvx.Variable(n + r + 1)
+        c = np.zeros(self.n + self.r + 1)
+        c[:self.r] = 1
         
-        c = np.zeros(n + r + 1)
-        c[:r] = 1
-        
-        objective = cvx.Minimize(cvx.quad_form(a, Q) - 4 * alphas[0] * a[-1])
-        constraints = [a[:-1] >= 0, c * a == 1]
-        prob = cvx.Problem(objective, constraints)
+        # Problem
+        zy = cvx.Variable(self.n + self.r + 1)
+        obj = cvx.Minimize(cvx.quad_form(zy, Q) - 4 * alphas[0] * zy[-1])
+        constr = [zy[:-1] >= 0, c * zy == 1]
+        prob = cvx.Problem(obj, constr)
 
         prob.solve(solver=cvx.CVXOPT)
         
+        # Results
         self.status = prob.status
-        score = np.array(a.value).flatten()
-        self.lamb = score[-1]
-        self.mu = score[r:-1]
-        self.ay = score[:r]
+        self.zy = np.array(zy.value).flatten()[:self.r]
+        #----------------------
+        # Parameters
+        Q, lamb_min = shift_spectrum(self.Qx)
         
-        self.ax = 1. / (2 * alphas[0]) * Qxinv.dot(alphas[1] * self.B.dot(self.ay) + score[-1] + score[r:-1])
+        c = np.ones(self.n)
         
-        a = cvx.Variable(n)
-        
-        c = np.ones(n)
-        
-        objective = cvx.Minimize(cvx.quad_form(a, self.Qx) - self.B.dot(self.ay).flatten() * a)
-        constraints = [a >= 0, c * a == 1]
+        # Problem
+        zx = cvx.Variable(self.n)
+        objective = cvx.Minimize(cvx.quad_form(zx, Q) - self.B.dot(self.zy).flatten() * zx)
+        constraints = [zx >= 0, c * zx == 1]
         prob = cvx.Problem(objective, constraints)
-
-        prob.solve(solver=cvx.CVXOPT)
-        self.ax = np.array(a.value).flatten()
-        
-    def _sdp(self, alphas):
-        n = self.Qx.shape[0]
-        r = self.Qy.shape[0]
-        
-        Qxinv = np.linalg.pinv(self.Qx)
-        Q1 = alphas[1] ** 2 * self.B.T.dot(Qxinv).dot(self.B) + 4 * alphas[0] * alphas[2] * self.Qy
-        Q2 = Qxinv
-        Q3 = Qxinv.sum(keepdims=True)
-        Q12 = alphas[1] * self.B.T.dot(Qxinv)
-        Q13 = alphas[1] * Q12.sum(axis=1, keepdims=True)
-        Q23 = Qxinv.sum(axis=1, keepdims=True)
-        
-        Q = np.vstack([
-            np.hstack([Q1, Q12, Q13]),
-            np.hstack([Q12.T, Q2, Q23]),
-            np.hstack([Q13.T, Q23.T, Q3])
-        ])
-        
-        q = np.zeros(n + r + 1)
-        q[-1] = -2. * alphas[0]
-        Qt = np.zeros((n + r + 2, n + r + 2))
-        Qt[:n + r + 1, :n + r + 1] = Q
-        Qt[-1, :-1] = q
-        Qt[:-1, -1] = q
-        A = np.zeros((1, n + r + 2))
-        A[0, :n] = 1
-        b = 1
-        
-        Z = cvx.Semidef(n + r + 2)
-        
-        objective = cvx.Minimize(cvx.trace(Qt * Z))
-        constraints = [cvx.trace(A.T.dot(A) * Z) == b ** 2]
-        prob = cvx.Problem(objective, constraints)
-
         prob.solve(solver=cvx.CVXOPT)
         
-        self.status = prob.status
+        # Results
+        self.zx = np.array(zx.value).flatten()
     
-    def _dual_woy(self, alphas):
-        n = self.Qx.shape[0]
-        r = self.Qy.shape[0]
-        
-        a = cvx.Variable(n + r + 1)
-        C = np.zeros((r + 1, n + r + 1))
-        C[0, :n] = 1
-        C[1:, :n] = alphas[1] * self.B.T
-        C[1:, n:n + r] = -np.eye(r)
-        C[1:, n + r] = -1
-        d = np.zeros(r + 1)
+    def _maxrel(self, alphas):
+        # Parameters
+        C = np.zeros((self.r + 1, self.n + self.r + 1))
+        C[0, :self.n] = 1
+        C[1:, :self.n] = alphas[1] * self.B.T
+        C[1:, self.n:self.n + self.r] = -np.eye(self.r)
+        C[1:, self.n + self.r] = -1
+        d = np.zeros(self.r + 1)
         d[0] = 1
-        Q = np.zeros((n + r + 1, n + r + 1))
-        Q[:n, :n] = self.Qx
         
-        objective = cvx.Minimize(alphas[0] * cvx.quad_form(a, Q) - a[-1])
-        constraints = [a[:-1] >= 0, C * a == d]
-        prob = cvx.Problem(objective, constraints)
-
+        Q, lamb_min = shift_spectrum(self.Qx, eps=1e-9)
+        
+        # Problem
+        z = cvx.Variable(self.n + self.r + 1)
+        obj = cvx.Minimize(alphas[0] * cvx.quad_form(z[:self.n], Q) - z[-1])
+        constr = [z[:-1] >= 0, C * z == d]
+        prob = cvx.Problem(obj, constr)
         prob.solve(solver=cvx.CVXOPT)
+        
+        # Results
         self.status = prob.status
-        score = np.array(a.value).flatten()
-        self.lamb = score[-1]
-        self.ax = score[:n]
+        self.zx = np.array(z.value).flatten()[:self.n]
+        self.zy = None
+    
+    def _minmax2(self, alphas):
+        # Parameters
+        c = np.ones(self.r)
+        
+        Q, lamb_min = shift_spectrum(self.Qy, eps=1e-9)
+        
+        # Problem
+        zy = cvx.Variable(self.r)
+        obj = cvx.Minimize(alphas[2] * cvx.quad_form(zy, Q) + 
+                           alphas[1] * self.B.mean(axis=0) * zy)
+        constr = [zy >= 0, c * zy == 1]
+        prob = cvx.Problem(obj, constr)
+        prob.solve(solver=cvx.CVXOPT)
+        
+        # Results
+        self.zy = np.array(zy.value).flatten()
+        #-------------------------------------
+        # Parameters
+        c = np.ones(self.n)
+        
+        Q, lamb_min = shift_spectrum(self.Qx, eps=1e-9)
+        
+        # Problem
+        zx = cvx.Variable(self.n)
+        obj = cvx.Minimize(alphas[0] * cvx.quad_form(zx, Q) - 
+                           alphas[1] * self.B.dot(self.zy).flatten() * zx)
+        constr = [zx >= 0, c * zx == 1]
+        prob = cvx.Problem(obj, constr)
+        prob.solve(solver=cvx.CVXOPT)
+        
+        # Results
+        self.status = prob.status
+        self.zx = np.array(zx.value).flatten()
     
     def get_topk_indices(self, k=10):
-        return self.score[self.B.shape[0]].argsort()[::-1][:k]
-    
-
-if __name__ == '__main__':
-    from sklearn.datasets import load_boston
-    X, y = load_boston(return_X_y=True)
-    qpfs = QPFS()
-    qpfs.fit(X, y)
-    print(qpfs.score)
+        return self.zx.argsort()[::-1][:k]
